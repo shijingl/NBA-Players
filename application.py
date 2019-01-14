25e8e7571d33d5d3e4ae75504b034ae1c5b5eab2
vagrant <vagrant@vagrant.vm>
2019-01-14 03:34:24 +0000

    fix string format bug

--- application.py
+++ application.py
@@ -213,10 +213,6 @@ def login():
 
 @app.route('/logout')
 def logout():
-    if login_session['provider'] == 'facebook':
-        fbdisconnect()
-        del login_session['facebook_id']
-
     if login_session['provider'] == 'google':
         gdisconnect()
         del login_session['gplus_id']
@@ -255,8 +251,8 @@ def gconnect():
 
     # Check that the access token is valid.
     access_token = credentials.access_token
-    url = ('https://www.google\n'
-           'apis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    url = ('https://www.googleapis'
+           '.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
 
