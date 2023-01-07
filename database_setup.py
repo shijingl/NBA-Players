63575b4300b464727123a2f2f789c654cb1b303f
shijingliu <liushijing123@gmail.com>
2023-01-07 16:54:59 -0500

    Resolve deprecated library issue

--- database_setup.py
+++ database_setup.py
@@ -1,7 +1,5 @@
-import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 
 Base = declarative_base()
